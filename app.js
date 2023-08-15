60b5a9e6fe73ca4908d57331fed8887793476a1c
weroszymaniak <weronika.szymaniak@hotmail.com>
2023-08-15 15:23:19 +0200

    update in routes and contacts

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express";
 import logger from "morgan";
 import cors from "cors";
 
-import contactsRouter from "./routes/api/contacts.js";
+import { contactsRouter } from "./routes/api/contacts.js";
 
 export const app = express();
 
@@ -10,7 +10,7 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
 app.use(cors());
-app.use(json());
+app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,28 +1,130 @@
 import express from "express";
 import Joi from "joi";
 
-//import funkcji z models/contacts
+import {
+  fetchContacts,
+  updateContactsData,
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+} from "../../models/contacts.js";
 
-const router = express.Router();
+export const contactsRouter = express.Router();
 
-router.get("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+contactsRouter.get("/", async (req, res, next) => {
+  try {
+    const contacts = await fetchContacts();
+    res.json({
+      status: "success",
+      code: 200,
+      data: { contacts },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      code: 500,
+      message: "Internal Server Error",
+    });
+  }
 });
 
-router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+contactsRouter.get("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+  try {
+    const contact = await fetchContacts(contactId);
+
+    if (!contact) {
+      res.status(404).json(`Contact not found`);
+    }
+
+    res.json({
+      status: "success",
+      code: 200,
+      data: { contact },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      code: 500,
+      message: "Internal Server Error",
+    });
+  }
 });
 
-router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+contactsRouter.post("/", async (req, res, next) => {
+  const { name, email, phone } = req.body;
+
+  if (!name) {
+    return res.status(400).json({ message: "missing required name field" });
+  }
+
+  if (!email) {
+    return res.status(400).json({ message: "missing required email field" });
+  }
+
+  if (!phone) {
+    return res.status(400).json({ message: "missing required phone field" });
+  }
+
+  try {
+    const newContact = await addContact({ name, email, phone });
+    res.status(201).json({
+      status: "success",
+      code: 201,
+      data: newContact,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      code: 500,
+      message: "Internal Server Error",
+    });
+  }
 });
 
-router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+contactsRouter.delete("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+
+  try {
+    await removeContact(contactId);
+    res.status(200).json({ message: "contact deleted" });
+  } catch (error) {
+    res.status(404).json({ message: "Not found" });
+  }
 });
 
-router.put("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
+contactsRouter.put("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+  const { name, email, phone } = req.body;
 
-module.exports = router;
+  if (!name && !email && !phone) {
+    return res.status(400).json({ message: "missing fields" });
+  }
+
+  const updatedFields = {};
+  if (name) updatedFields.name = name;
+  if (email) updatedFields.email = email;
+  if (phone) updatedFields.phone = phone;
+
+  try {
+    const updatedContact = await updateContact(contactId, updatedFields);
+
+    if (!updatedContact) {
+      return res.status(404).json({ message: "Not found" });
+    }
+
+    res.status(200).json({
+      status: "success",
+      code: 200,
+      data: updatedContact,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      code: 500,
+      message: "Internal Server Error",
+    });
+  }
+});
