9beb61ab31a26cab596e06382768caab8b80d2cf
weroszymaniak <weronika.szymaniak@hotmail.com>
2023-08-21 20:17:58 +0200

    schema and mongoose added

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 import { app } from "./app.js";
+import mongoose from "mongoose";
+import { config } from "dotenv";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Database connection successful");
+
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1);
+  });
